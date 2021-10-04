b060502f005443a2b3faa701d2e1af270ad08bdf
Calvin Roberts <roberts.calvin85@gmail.com>
2021-10-04 23:56:37 +0100

    Style variables to move and rotate

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const hr = document.querySelector('#hr'),
   mn = document.querySelector('#mn'),
   sc = document.querySelector('#sc');
 
-let day = new Date();
-let hh = day.getHours() * 30;
-let mm = day.getMinutes() * deg;
-let ss = day.getSeconds() * deg;
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+}, 1000);
